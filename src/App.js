60fcd6a274bc0fd1ab29782af1c710496f6d9dcf
oluwadareseyi <oluwadareseyii@gmail.com>
2021-03-04 11:44:12 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import gsap from "gsap";
+import { Draggable } from "gsap/all";
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const arr = [...new Array(10)].map((arr, i) => (arr = i));
+  const [index, setIndex] = useState(0);
+
+  gsap.registerPlugin(Draggable);
+  useEffect(() => {
+    const colors = ["#f38630", "#6fb936", "#ccc", "#6fb936"];
+    gsap.set(".box", {
+      backgroundColor: (i) => colors[i % colors.length],
+    });
+  }, []);
+
+  const nextButtonClick = () => {
+    if (!gsap.isTweening(".carousel")) {
+      if (index === arr.length - 1) return;
+      gsap.to(".carousel", {
+        duration: 0.25,
+        xPercent: (-100 / arr.length) * (index + 1),
+        onComplete: () => setIndex(index + 1),
+      });
+    }
+  };
+
+  const prevButtonClick = () => {
+    if (!gsap.isTweening(".carousel")) {
+      if (index === 0) return;
+      gsap.to(".carousel", {
+        duration: 0.25,
+        ease: "Linear.easeNone",
+        xPercent: (-100 / arr.length) * (index - 1),
+        onComplete: () => setIndex(index - 1),
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="carousel" style={{ width: `${100 * arr.length}vw` }}>
+          {arr.map((item) => (
+            <div className="box" key={item}>
+              {item}
+            </div>
+          ))}
+        </div>
+
+        <div className="controls">
+          <button
+            disabled={index === arr.length - 1}
+            id="nextButton"
+            onClick={nextButtonClick}
+          >
+            Next
+          </button>
+          <button
+            disabled={index === 0}
+            id="prevButton"
+            onClick={prevButtonClick}
+          >
+            Previous
+          </button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
